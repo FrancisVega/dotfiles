2f4b6e03da2726d8e279653e1eb1013d979ef95f
Francis <hisconer@gmail.com>
2021-01-01 09:32:55 +0100

    Refactor

--- install.sh
+++ install.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env sh
 
-FILE=~/.vimrc
-if [ -f "$FILE" ]; then
-    echo "$FILE exists. Remove it first"
-else 
-    echo "download .vimrc"
-    curl https://raw.githubusercontent.com/FrancisVega/dotfiles/master/vimrc.symlink --output ~/.vimrc
-fi
+# This script download my dotfiles and install (copy) them if they don't exists
+# in the system.
+#
+# Use:
+# curl https://raw.githubusercontent.com/FrancisVega/dotfiles/master/install.sh | bash
+
+files=(vimrc zshrc)
+github_url=https://raw.githubusercontent.com/FrancisVega/dotfiles/master
+
+download_dotfile () {
+    if [ -f $2 ]; then
+        echo $2 exists. Remove it first.
+    else 
+        echo "install $2"
+        curl $1 --output $2
+    fi
+}
+
+for dotfile in ${files[@]}
+do
+    download_dotfile "$github_url/$dotfile.symlink" ~/.$dotfile
+done
