39de06f91fc5528e3fa136a64812962aba45fcaa
FrancisVega <hisconer@gmail.com>
2025-10-30 17:44:22 +0100

    Add initial configuration files and aliases for shell environment

--- nvim/lua/hisco/core/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.termguicolors = true
-vim.cmd([[ colorscheme lleyou ]])
-vim.cmd([[ set background=dark ]])
-vim.cmd([[
-  hi Pmenu guibg=clear
-  hi Visual guifg=#000000 guibg=#FFFFFF gui=none
-]])
